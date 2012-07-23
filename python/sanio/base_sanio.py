4ce70013bfbd3b332d17eeaeb5f29ebcb19d34a9
Erik Smartt <code@eriksmartt.com>
2012-07-23 15:28:47 -0500

    supporting a new approach for passing multiple cleaners

--- python/sanio/base_sanio.py
+++ python/sanio/base_sanio.py
@@ -17,31 +17,57 @@ class BaseSanio(object):
         return self
 
     def _clean(self, data):
-        out = None
+        """
+        Applies your cleaner to your data.
+        """
+        if isinstance(data, (dict,)):
+            out = dict()
+        else:
+            out = None
+
+        _first = True
 
         if self.cleaner is not None:
-            try:
-                if isinstance(data, (dict,)):
-                    for k, v in data.items():
-                        out[k] = self.cleaner.clean(k, v)
+            if not isinstance(self.cleaner, (list,)):
+                self.cleaner = list(self.cleaner)
 
-                else:
-                    out = self.cleaner.clean(None, data)
+            for obj in self.cleaner:
+                try:
+                    if isinstance(data, (dict,)):
+                        if _first:
+                            for k, v in data.items():
+                                out[k] = obj.clean(k, v)
+                            _first = False
 
-            except AttributeError:
-                # self.cleaner is probably None
-                pass
+                        else:
+                            for k, v in out.items():
+                                out[k] = obj.clean(k, v)
 
-            except TypeError:
-                # self.cleaner.clean isn't callable
-                pass
+                    else:
+                        if _first:
+                            out = obj.clean(None, data)
+                            _first = False
 
-        if out is not None:
+                        else:
+                            out = obj.clean(None, out)
+
+                except AttributeError:
+                    # self.cleaner is probably None
+                    pass
+
+                except TypeError:
+                    # self.cleaner.clean isn't callable
+                    pass
+
+        if out:
             return out
         else:
             return data
 
     def _filter(self, data):
+        """
+        Applies your filterer to your data.
+        """
         out = None
 
         if self.filter is not None:
@@ -63,9 +89,13 @@ class BaseSanio(object):
             return data
 
     def clean(self, key=None, value=None):
+        # This is a dummy cleaning function. It's *not* how you run your cleaner (that's _clean());
+        # It's how this object cleans.
         return value
 
     def filter(self, d):
+        # This is a dummy filter function. It's *not* how you run your filters (that's _filter());
+        # It's how this object filters.
         return d
 
     def next(self):
